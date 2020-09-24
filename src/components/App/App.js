d7292f94d2de203fde94c477156399e1351bed29
Ajay Nayr <ajaynayr@gmail.com>
2020-09-24 11:04:03 -0500

    Added a basic boilerplate for router and redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { connect } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Prime Pizza</h1>
-        </header>
-        <br/>
-        <img src="images/pizza_photo.png"/>
-        <p>Pizza is great.</p>
-      </div>
+      <Router >
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Prime Pizza</h1>
+          </header>
+          <br/>
+          <img src="images/pizza_photo.png"/>
+          <p>Pizza is great.</p>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+// REDUX
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    combineReducers({
+        //enter reducer names here,
+
+    }),
+    applyMiddleware(logger)
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root')); 

252ef7982c73315f5734da097cc6b6872882c166
Cooper Doten <65552627+CooperDoten@users.noreply.github.com>
2020-09-25 11:41:49 -0500

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Pizzas from '../../components/Pizzas/Pizzas';
 import Cart from '../../components/Cart/Cart';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
 
 class Home extends Component{
     render(){

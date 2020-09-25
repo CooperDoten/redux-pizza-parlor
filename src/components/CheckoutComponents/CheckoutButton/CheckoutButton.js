06b1169fb2e137cd091048819c45d7b0ffcd214c
Ajay Nayr <ajaynayr@gmail.com>
2020-09-25 11:22:01 -0500

    Fleshed out the checkout button

--- src/components/CheckoutComponents/CheckoutButton/CheckoutButton.js
+++ src/components/CheckoutComponents/CheckoutButton/CheckoutButton.js
@@ -1,26 +1,38 @@
+
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import Axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
 class CheckoutButton extends Component {
 
     handleCheckout = () => {
-     
-    }
+        let order = {
+          pizzas: this.props.reduxState.cartReducer,
+        };
+        console.log('Checking Out WOOOO!');
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleCheckout}>Checkout</button>
-            </div>
-        )
+        let completeOrder = Object.assign(this.props.reduxState.customerInfo, order)
+        console.log(completeOrder);
+        Axios.post('/api/order', completeOrder)
+          .then(response => {
+            this.props.dispatch({
+              type: 'CHECKOUT'
+            });
+            //maybe an alert that order has been submitted?
+            this.props.history.push('/');
+          })
     }
-}
+    render() {
+        return(
+            <Button variant="contained" size="small" onClick={() => this.handleCheckout()}>Checkout</Button>
+        );
+    };
+};
 
 const mapStateToProps = (reduxState) => {
-    console.log(reduxState);
   return {
-      reduxState
+    reduxState
   }
 }
-  
-export default connect(mapStateToProps)(CheckoutButton);
+export default connect(mapStateToProps)(withRouter(CheckoutButton));
